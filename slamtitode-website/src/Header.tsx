85dd2e84026a10610c2af82262c1e40302c576a6
Kaven101 <iikevinl360ii@gmail.com>
2025-06-12 08:58:40 +1000

    General Header
    Header near complete:
    - Only complete for desktop devices

---
+++ slamtitode-website/src/Header.tsx
@@ -0,0 +1,42 @@
+import slamLogo from "/slamlogo.svg";
+import "./Header.css";
+
+function Header() {
+  return (
+    <>
+      <header>
+        <a href="#" target="_blank">
+          <img src={slamLogo} className="logo" alt="Slamtidote Logo" />
+        </a>
+        <nav>
+          <a className="nav-link" href="#">
+            Our Story
+          </a>
+          <a className="nav-link" href="#">
+            Workshops
+          </a>
+          <a className="nav-link" href="#">
+            Blogs
+          </a>
+          <a className="nav-link" href="#">
+            Events
+          </a>
+          <a className="nav-link" href="#">
+            Socials
+          </a>
+          <a className="nav-link" href="#">
+            Media Archive
+          </a>
+          <a className="nav-link" href="#">
+            Charities
+          </a>
+          <a className="nav-link" href="#">
+            Podcast
+          </a>
+        </nav>
+      </header>
+    </>
+  );
+}
+
+export default Header;
