6b9ceaac0b40b0a15611a98df4f87b143802e25f
Kaven101 <iikevinl360ii@gmail.com>
2025-06-24 20:12:55 +1000

    Created homepage container
    Created Homepage container

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,30 @@
-import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <body>
+        <section className="homepage-container">
+          <div className="subheading-container">
+            <div className="subheading">
+              Putting the Lit in ethnic, queer and femme Literature (and allies)
+            </div>
+            <div className="close-button">
+              <span></span>
+              <span></span>
+            </div>
+          </div>
+          <div className="heading-container">
+            <h1>Slamtitote</h1>
+          </div>
+          <div className="button-container">
+            <button className="join">Join us</button>
+            <button className="next-event">Take me to the next event</button>
+          </div>
+        </section>
+      </body>
+    </>
+  );
 }
 
 export default App;
