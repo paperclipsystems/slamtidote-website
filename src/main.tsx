452e86104da48863fafb6636d48d9c90ca2ffe90
saanvitel <telukotisaanvi@gmail.com>
2025-06-25 20:08:08 +1000

    refactoring react components and cleaning up css

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/index.css";
 import App from "./App.tsx";
-import Header from "./components/Header.tsx";
 import "./styles/Header.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Header />
     <App />
   </StrictMode>
 );
