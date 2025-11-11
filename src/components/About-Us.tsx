b1b8a5a65497d4942cda2b3cc6bcfd8ad094ea13
Hafsah Yasir <s4093222@student.rmit.edu.au>
2025-11-11 21:49:34 +1100

    feat: created base page for About Us and filled it with random information...

---
+++ src/components/About-Us.tsx
@@ -0,0 +1,21 @@
+
+function AboutUs() {
+    return (
+        <div className="about__container">
+            <h1>About Us</h1>
+            <p>
+                SLAMTIDOTE is dedicated to celebrating and promoting
+                the voices of marginalized communities through literature, art, and culture.
+            </p>
+            <p>
+                Our mission is to create a platform that amplifies these voices and fosters a sense of belonging and empowerment.
+            </p>
+
+            <p>
+                XYZ is a community-driven initiative that aims to uplift and support underrepresented voices in the literary and artistic landscape.
+            </p>
+        </div>
+    );
+}
+
+export default AboutUs;
